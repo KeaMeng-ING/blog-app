a9b35050bcb6886b061f5190f5ba24e99685ff8a
KeaMeng <2023484ing@aupp.edu.kh>
2025-03-19 14:02:14 +0700

    Cors config

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const port = 3000;
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
 const commentRoutes = require("./routes/commentRoutes");
+const cors = require("cors"); // Import CORS
 
+app.use(cors()); // Enable CORS
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello World",
+    message: "Hello Worldd",
   });
 });
 app.use("/api/users", userRoutes);
